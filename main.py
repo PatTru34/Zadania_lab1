0278e1496f16b6cfc43d1d7529864b493361d56e
PatTru34 <PatTru34>
2021-03-04 14:36:02 +0100

    Zadanie 2

--- main.py
+++ main.py
@@ -1,11 +1,74 @@
-#Zadanie 1
+# Zadanie 1
+#
+# a,b=5,6
+# napis_1,napis_2='Kot','Pies'
+# x,z=3.5,2.8
+# print(a)
+# print(b)
+# print(napis_1)
+# print(napis_2)
+# print (x)
+# print(z)
 
-#a,b=5,6
-#napis_1,napis_2='Kot','Pies'
-#x,z=3.5,2.8
-#print(a)
-#print(b)
-#print(napis_1)
-#print(napis_2)
-#print (x)
-#print(z)
+
+# Zadanie 2
+# a,b,c=4,6,2
+# dodawanie=a+b
+# odejmowanie=b-c
+# mnozenie=a*c
+# dzielenie=b/c
+# reszta=b%a
+# potega=c**a
+#
+# print(dodawanie)
+# print(odejmowanie)
+# print(mnozenie)
+# print(dzielenie)
+# print(reszta)
+# print(potega)
+
+
+# Zadanie 3
+
+# a=5
+# a+=5
+# print(a)
+# b=5
+# b-=2
+# print(b)
+# c=5
+# c*=5
+# print(c)
+# d=5
+# d/=2
+# print(d)
+# x=5
+# x%=2
+# print(x)
+# y=5
+# y**=2
+# print(y)
+
+# Zadanie 5
+
+# imie="PATRYK"
+# nazwisko="TRUCHAN"
+# print(imie.capitalize()+" "+nazwisko.capitalize())
+
+# Zadanie 6
+
+# tekst=" la la la la la viras la la vi vi viras"
+# cel="la"
+# count=tekst.count(cel)
+# print(count)
+
+# Zadanie 7
+# string="Coco Jambo"
+# print("druga litera napisu: "+string[1]+"\nostatnia litera napisu: "+string[-1])
+
+# Zadanie 8
+# tekst="la la la la la viras la la vi vi viras"
+# x=tekst.split(" ")
+# print(x)
+
+# Zadanie 9

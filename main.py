9d69c2b84bed0481923e5e070071a5355e3de964
PatTru34 <PatTru34>
2021-03-04 14:05:08 +0100

    Zadanie 2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #Zadanie 1
 
-a,b=5,6
-napis_1,napis_2='Kot','Pies'
-x,z=3.5,2.8
-print(a)
-print(b)
-print(napis_1)
-print(napis_2)
-print (x)
-print(z)
+#a,b=5,6
+#napis_1,napis_2='Kot','Pies'
+#x,z=3.5,2.8
+#print(a)
+#print(b)
+#print(napis_1)
+#print(napis_2)
+#print (x)
+#print(z)

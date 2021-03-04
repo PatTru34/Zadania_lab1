506a0c98e4725671fab5574d6584087ddee8bb9a
PatTru34 <PatTru34>
2021-03-04 14:01:16 +0100

    Zadanie 1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-napis = 'Ala ma kota'
-print(napis)
-print("tekst")
+#Zadanie 1
+
+a,b=5,6
+napis_1,napis_2='Kot','Pies'
+x,z=3.5,2.8
+print(a)
+print(b)
+print(napis_1)
+print(napis_2)
+print (x)
+print(z)
